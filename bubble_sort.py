ef6f9c8f06c221b27ade9c121e08a632887e5714
Adam Kraś <adam.kras.stud@pw.edu.pl>
2023-03-13 18:37:19 +0100

    Implemented bubble sort algorithm

---
+++ bubble_sort.py
@@ -0,0 +1,8 @@
+def bubble_sort(lista):
+    items_num = len(lista)
+    while(items_num > 0):
+        for item in range(items_num - 1):
+            if lista[item] > lista[item + 1]:
+                lista[item], lista[item + 1] = lista[item + 1], lista[item]
+        items_num -= 1
+    return lista
