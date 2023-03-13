886c6136f9cd3082480ab2c0ec3fe6e8f56c3a89
Adam Kraś <adam.kras.stud@pw.edu.pl>
2023-03-13 19:17:26 +0100

    Implemented shell sort algorithm

---
+++ shell_sort.py
@@ -0,0 +1,16 @@
+from re import I
+
+
+def shell_sort(lista):
+    items_num = len(lista)
+    interval = items_num // 2
+    while interval > 0:
+        for i in range(interval, items_num):
+            temp = lista[i]
+            j = i 
+            while j >= interval and lista[j - interval] > temp:
+                lista[j] = lista[j - interval]
+                j -= interval
+            lista[j] = temp
+        interval //= 2
+    return lista
