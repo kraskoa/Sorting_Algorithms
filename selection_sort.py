5b6a33d36d128c798b2eb8325c6a4361caee6588
Adam Kraś <adam.kras.stud@pw.edu.pl>
2023-03-15 16:45:19 +0100

    Removed testing clause from algorithm file

--- selection_sort.py
+++ selection_sort.py
@@ -8,13 +8,3 @@ def selection_sort(array):
                 maks_index = ii
         array[maks_index], array[size - 1] = array[size - 1], array[maks_index]
         maks = array[0]
-
-
-def main():
-    my_list = [2, 51, 1, 3, 100, 13, 12, 2, -9, -8, 0]
-    selection_sort(my_list)
-    print(my_list)
-
-
-if __name__ == "__main__":
-    main()
