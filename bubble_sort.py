f3bf411cfe085e09d817b95a72dbf6ef2b12d4d9
Adam Kraś <adam.kras.stud@pw.edu.pl>
2023-03-19 13:12:09 +0100

    Repaired syntax in bubble_sort function

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,6 +1,6 @@
 def bubble_sort(lista):
     items_num = len(lista)
-    while(items_num > 0):
+    while items_num > 0:
         for item in range(items_num - 1):
             if lista[item] > lista[item + 1]:
                 lista[item], lista[item + 1] = lista[item + 1], lista[item]
