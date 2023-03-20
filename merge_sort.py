0658c59ab1ead2c9b61f9155c5a7764edd472d2f
Krzysztof Król <krzysztof.krol.stud@pw.edu.pl>
2023-03-20 17:03:16 +0100

    Fixed merge_sort Now merge_sort don't use append() and pop() methods of an array

--- merge_sort.py
+++ merge_sort.py
@@ -1,38 +1,27 @@
 def merge_sort(array):
-    array2 = []
-    begin = 0
-    end = len(array)
-    mid = (begin + end) // 2
-    if mid != begin:
-        first_half = array[0:mid]
-        second_half = array[mid:end]
+    if len(array) > 1:
+        end = len(array)
+        mid = end // 2
+        first_half = array[:mid]
+        second_half = array[mid:]
         first_half = merge_sort(first_half)
         second_half = merge_sort(second_half)
-    if len(array) > 1:
+        j = k = 0
         for i in range(end):
-            if len(first_half) == 0 and len(second_half) == 0:
-                return array2
-            elif len(first_half) == 0:
-                array2.append(second_half[0])
-                second_half.pop(0)
-            elif len(second_half) == 0:
-                array2.append(first_half[0])
-                first_half.pop
-            elif first_half[0] <= second_half[0]:
-                array2.append(first_half[0])
-                first_half.pop(0)
+            if j >= len(first_half) and k >= len(second_half):
+                return array
+            elif j >= len(first_half):
+                array[i] = second_half[k]
+                k += 1
+            elif k >= len(second_half):
+                array[i] = first_half[j]
+                j += 1
+            elif first_half[j] <= second_half[k]:
+                array[i] = first_half[j]
+                j += 1
             else:
-                array2.append(second_half[0])
-                second_half.pop(0)
-        return array2
+                array[i] = second_half[k]
+                k += 1
+        return array
     else:
         return array
-
-
-def main():
-    my_list = [23, 11, 2, 134, 8, 12]
-    print(merge_sort(my_list))
-
-
-if __name__ == "__main__":
-    main()
